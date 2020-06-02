e33b6810cba333f5898255a1945cb8da7b6df284
Reuben Mathew <intelphysic@gmail.com>
2020-06-02 17:23:30 -0400

    button customizable component completed

---
+++ components/buttons/buttons.md
@@ -0,0 +1,24 @@
+# Vercel Buttons
+
+## Usage
+
+```html
+    <Button type="<Type>" mod="<Modifier>">Action</Button>
+    <SmallButton type="<Type>" mod="<Modifier>">Action</SmallButton>
+```
+#### Example
+```html
+    <Button type="warning" mod="shadow">Action</Button>
+    <SmallButton type="warning" mod="shadow">Action</SmallButton>
+```
+
+### Types
+- primary
+- secondary
+- success
+- error
+- warning
+
+### Modifiers
+- shadow
+
---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    colors: {
+      white: '#FFF',
+      accent1: '#FAFAFA',
+      accent2: '#EAEAEA',
+      accent3: '#999',
+      accent4: '#888',
+      accent5: '#666',
+      accent6: '#444',
+      accent7: '#333',
+      accent8: '#111',
+      black: '#000',
+      errorlight: '#FF1A1A',
+      error: '#E00',
+      errordark: '#C00',
+      successlight: '#3291FF',
+      success: '#0070F3',
+      successdark: '#0366D6',
+      warninglight: '#F7B955',
+      warning: '#F5A623',
+      warningdark: '#F49B0B',
+      alert: '#FF0080',
+      purple: '#F81CE5',
+      violet: '#7928CA',
+      cyan: '#79FFE1',
+      'primary': '#000',
+      'secondary': 'FFF'
+    },
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+        akronim: ['Akronim', 'cursive'],
+      },
+    },
+  },
+  variants: {},
+  plugins: []
+}
